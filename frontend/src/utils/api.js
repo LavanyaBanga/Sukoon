c3f5882ed74a0c79aa39170917e9cf2b8c018d72
Lavanya Banga <lavanyabanga14@gmail.com>
2026-08-14 21:59:13 +0530

    frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    (import.meta.env.DEV
-      ? 'http://localhost:5000/api'
-      : 'https://sukoon-backend-chel.onrender.com/api'),
+    'https://sukoon-backend-chel.onrender.com/api',
 });
 
 api.interceptors.request.use(
